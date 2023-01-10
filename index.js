e74f385a38abb09d9adfc3b8b7b4703c74e786a0
Denesh <denesh@u.nus.edu>
2023-01-10 11:52:36 +0800

    Logging out the hello

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const exec = require('@actions/exec')
 const run = async () => {
     try {
         const name = core.getInput('name');
-        await exec.exec(`Hello ${name}`);
+        // await exec.exec(`Hello ${name}`);
+        console.log(`Hello ${name}`)
     } catch(error) {
         core.setFailed(error.message);
     }
