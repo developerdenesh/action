5b6019d68a647c5e02a20bc29372f23554c7fec1
Denesh <denesh@u.nus.edu>
2023-01-10 11:27:03 +0800

    Adding code to say hello

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+
+const run = async () => {
+    try {
+        const name = core.getInput('name');
+        await exec.exec(`Hello ${name}`);
+    } catch(error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
